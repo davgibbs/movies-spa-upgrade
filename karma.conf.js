2d1fedd099ba0694efad3df647b516da7f21bf0e
David Gibbons <dgibbons@eircom.net>
2018-10-19 12:10:06 +0100

    Removed package

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,6 @@ module.exports = function(config) {
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
-
     // list of files / patterns to load in the browser
     files: [
         './node_modules/angular/angular.min.js',
